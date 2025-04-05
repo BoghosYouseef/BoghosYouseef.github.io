6a0fdcab2a3bea9a10e385bc4f29779c05fb1a4d
Boghos Youseef <boghos.youseef@outlook.com>
2025-04-05 13:02:00 +0200

    added coding blog and navbar

--- CodingBlog/views.py
+++ CodingBlog/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 # Create your views here.
+
+
+def home(request):
+    context = {}
+    return render(request=request, template_name="mainCodingBlog.html", context=context)
+
+    # return HttpResponse("SUCCESS!")
